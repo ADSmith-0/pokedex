9e242169e51ba9aa5873e8ecf00c8fa73fe2aac1
ADSmith-0 <adam00199382818@gmail.com>
2022-08-18 14:04:25 +0100

    update: header css to be sticky and pokemon list now comes from API into card components

--- src/AllPokemon.js
+++ src/AllPokemon.js
@@ -1,7 +1,22 @@
+import { useState, useEffect } from "react";
+import { getPokemon } from "./util";
+import Card from "./components/Card";
+
 const AllPokemon = () => {
+    const [pokemon, setPokemon] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const first50Pokemon = await getPokemon(0, 50);
+            setPokemon(first50Pokemon);
+        })();
+    }, [])
+    
     return (
         <>
-            <p>All Pokemon</p>
+            {pokemon.map(({name, url}, id) => (
+                <Card key={id} name={name} url={url} />
+            ))}
         </>
     )
 }
---
+++ src/components/Card.js
@@ -0,0 +1,6 @@
+const Card = ({ name, url }) => {
+    return (
+        <p>{name}</p>
+    )
+}
+export default Card;
