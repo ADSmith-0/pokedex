57248dcf7b8119fa33c28576cb474c17aef68aee
ADSmith-0 <adam00199382818@gmail.com>
2022-08-20 23:11:32 +0100

    added: autocomplete functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,41 @@
 import pokemonNames from '../PokemonNames.json';
 import '../css/SearchBar.css';
+import { useState } from 'react';
 
 const SearchBar = () => {
+
+    const [ suggestions, setSuggestions ] = useState(pokemonNames);
+    const [ isAutocompleteVisible, setIsAutocompleteVisible ] = useState(false);
+    const [ inputValue, setInputValue ] = useState("");
+    
+    const autoComplete = searchTerm => {
+        if(searchTerm){
+            setSuggestions(pokemonNames.filter(name => name.toLowerCase().includes(searchTerm.toLowerCase())));
+            setIsAutocompleteVisible(true);
+        }else{
+            setIsAutocompleteVisible(false);
+        }
+    }
+
+    const handleSearchInput = e => {
+        const searchTerm = e.target.value;
+        setInputValue(searchTerm);
+        autoComplete(searchTerm);
+    }
+
+    const populateSearchBar = e => {
+        setInputValue(e.target.innerText);
+        setIsAutocompleteVisible(false);
+    }
+
   return (
     <section id="searchbar-component">
-        <input id="search-name" type="text" placeholder="Search"/>
+        <input id="search-name" type="text" value={inputValue} onInput={handleSearchInput} placeholder="Search"/>
+        <ul id="search-autocomplete" className={isAutocompleteVisible ? "visible" : ""}>
+            {suggestions.map(suggestion => (
+                <li className="autocomplete-suggestion" onClick={populateSearchBar}>{suggestion}</li>
+            ))}
+        </ul>
         <button id="search-btn"><span id="search-icon">⚲</span></button>
     </section>
   );
