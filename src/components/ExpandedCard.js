335bd4ee7b71cd192cd45db142aa8d7fcca3cef0
ADSmith-0 <adam00199382818@gmail.com>
2022-08-21 22:50:38 +0100

    update: added pokemon types

--- src/components/ExpandedCard.js
+++ src/components/ExpandedCard.js
@@ -2,26 +2,30 @@ import { formatId } from '../util';
 import Loading from './Loading';
 import '../css/ExpandedCard.css';
 
-const ExpandedCard = ({ info, setExpandedCardInfo }) => {
+const ExpandedCard = ({ info, setExpandedCardInfo, backEnabled=true }) => {
 
     if (info !== undefined){
         return (
             <section id="blocker">
                 <section className="expanded-card">
-                    <button className="back-btn" onClick={() => setExpandedCardInfo(undefined)}>
-                        <img className="back-arrow" src="/img/left-arrow-white-96x96.png" alt="back arrow" height="40" width="40"/>
-                    </button>
+                    {
+                        backEnabled &&
+                        <button className="back-btn" onClick={() => setExpandedCardInfo(undefined)}>
+                            <img className="back-arrow" src="/img/left-arrow-white-96x96.png" alt="back arrow" height="40" width="40"/>
+                        </button>
+                    }
                     <section className="expanded-card-info">
                         <img src={info.sprites.front_default} height="150" width="150" alt={info.name+" sprite"}/>
                         <div className="pokemon-identification">
                             <p>{formatId(info.id)}</p>
                             <p>{info.name.toTitleCase()}</p>
                         </div>
+                        <p className="pokemon-types">Types: {info.types.reduce((acc, val) => acc+ ` ${val.type.name},`, "").slice(0,-1)}</p>
                         <section className="pokemon-stats-list">
                             {
                                 info.stats.map((stat, id) => (
                                     <div key={id} className="pokemon-stat">
-                                        <label>{stat.stat.name}:</label>
+                                        <label>{stat.stat.name.replace("-", " ")}:</label>
                                         <p>{stat.base_stat}</p>
                                     </div>
                                 ))
